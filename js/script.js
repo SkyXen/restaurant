8625aab36099e7b0875dba45c3eacaa265e4cfe6
Akash Kumar <20ee01071@iitbbs.ac.in>
2022-06-22 13:29:02 +0530

    ok

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+
+    $("#navbarToggle").blur(function(event) {
+        var screenWidth = window.innerWidth;
+        if (screenWidth < 768) {
+            $("#collapsable-nav").collapse('hide');
+        }
+    });
+    $("#navbarToggle").click(function(event) {
+        $(event.target).focus();
+    });
+});
